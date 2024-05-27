6c78832c5335b279ab4aa68cbc12566cd6260967
monaceKing <leroyjulien055@gmail.com>
2024-05-27 12:25:48 +0200

    apres ajout de la bibliotheque material avec ng add @angular/material, ajout des import MatTab,MAtInput...dans le app.config.ts et les appeler dans le .ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabGroup, MatTabsModule } from '@angular/material/tabs';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatTabsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ngOnglets';
+  @ViewChild(MatTabGroup)
+  tabGroup!: MatTabGroup;
+  formGroup: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formGroup = this.fb.group({
+      nom: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      adresse: ['', Validators.required],
+      telephone: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.formGroup.valid) {
+      console.log(this.formGroup.value);
+      this.formGroup.reset();
+      this.tabGroup.selectedIndex = 0; // Revenir au premier onglet
+    } else {
+      console.log("Le formulaire n'est pas valide");
+    }
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAnimationsAsync()]
 };
